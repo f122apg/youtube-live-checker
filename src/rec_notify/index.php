e1677e0c49b6bb12cf7a194014be472fa2416d1c
f122apg <f122apg@gmail.com>
2022-10-09 06:39:22 +0900

    cloud functionのgen2に対応

--- src/rec_notify/index.php
+++ src/rec_notify/index.php
@@ -1,20 +1,23 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Google\CloudFunctions\CloudEvent;
+use Google\CloudFunctions\FunctionsFramework;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Utils;
 
+use F122apg\YoutubeLiveChecker\App;
 use F122apg\YoutubeLiveChecker\AWS\Sns;
 
-function main(CloudEvent $cloudevent): ResponseInterface
-{
-    $pubsubData = base64_decode($cloudevent->getData()['message']['data']);
-    $notifyContent = json_decode($pubsubData);
+FunctionsFramework::http('notify', 'notify');
 
-    if (!empty($notifyContent)) {
-        $sns = new Sns($notifyContent->title, $notifyContent->contentId);
+function notify(ServerRequestInterface $request): ResponseInterface
+{
+    $queries = $request->getQueryParams();
+
+    if (!empty($queries) && isset($queries['notify']) && $queries['notify']) {
+        $sns = new Sns($queries['title'], $queries['contentId']);
         $sns->publish();
 
         return (new Response())
