caa3c29fed1d0a02d239be622f0034b99c6ec66f
f122apg <f122apg@gmail.com>
2022-10-12 23:43:29 +0900

    yt-dlpのオプションを追加＆ダウンロードしたファイルはzipでCloud Storageに配置する

--- gcp/batch/job.sh
+++ gcp/batch/job.sh
@@ -2,7 +2,7 @@
 
 # install dependencies
 apt update
-apt install -y curl xz-utils
+apt install -y curl xz-utils zip
 
 # install ffmpeg ffprobe
 curl -LO https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
@@ -15,7 +15,10 @@ chmod +x yt-dlp
 
 # live download
 mkdir /work
-./yt-dlp -P /work --live-from-start ${CONTENT_ID}
+./yt-dlp -P /work --live-from-start --write-all-thumbnails --audio-quality 0 --keep-video -f bestvideo*+bestaudio/best ${CONTENT_ID}
+
+# compress videos
+zip ${CONTENT_ID}.zip /work/*
 
 # move to bucket
-mv /work/* ${OUTPUT_PATH}
+mv ${CONTENT_ID}.zip ${OUTPUT_PATH}
